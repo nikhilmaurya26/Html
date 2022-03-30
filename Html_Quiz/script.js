d782a922f15cb0f0c177f13704f5ac2b6fe2fdb4
Nikhil <nikhilsingh0926@gmail.com>
2022-03-30 12:56:23 +0530

    Inittial_commit

--- Html_Quiz/script.js
+++ Html_Quiz/script.js
@@ -2,4 +2,62 @@
 const startButton =document.getElementById("start-btn")
 const nextButton =document.getElementById("next-btn")
 
-const startButton =document.getElementById("next-btn")
+const questionContainerElement =document.getElementById("question-container")
+const questionElement =document.getElementById("question")
+const answerButtonElements =document.getElementById("answer-buttons")
+
+let shuffedQuestions,currectQuestionIndex;
+let quizScore=0;
+
+
+function selectAnswer(e){
+    const selectedButton=e.target
+    const correct=selectedButton.dataset.correct
+
+    setStatusClass(document.body.correct)
+    Array.from()
+}
+
+function setStatusClass(element,correct){
+    clearStatusClass(element)
+    if(correct){
+        element.classsList.add("correct")
+    }else{
+        element.classsList.add('wrong')
+    }
+
+}
+
+function clearStatusClass(element){
+    element.classsList.remove('correct')
+    element.classsList.remove('wrong')
+}
+const question=[
+    {
+    question:"Which one of these is Javascript farmewor",
+    answers :[
+        {text:'python',correct:false},
+        {text:'react',correct:true},
+        {text:'django',correct:false},
+        {text:'ecillipse',correct:false},
+    ],
+},
+{
+    question:"Who is prime minister of india",
+    answers :[
+        {text:'Narendra modi',correct:true},
+        {text:'nehru',correct:true},
+        {text:'django',correct:false},
+        {text:'ecillipse',correct:false},
+    ],
+},
+{
+    question:"3x4=?",
+    answers :[
+        {text:'3',correct:false},
+        {text:'12',correct:true},
+        {text:'4',correct:false},
+        {text:'18',correct:false},
+    ],
+},
+]
