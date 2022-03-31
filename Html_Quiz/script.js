c8b74a1b0e2d62ccc0e495d8cf833dc85916a08d
Nikhil <nikhilsingh0926@gmail.com>
2022-03-31 23:34:10 +0530

    Java_script

--- Html_Quiz/script.js
+++ Html_Quiz/script.js
@@ -4,19 +4,81 @@ const nextButton =document.getElementById("next-btn")
 
 const questionContainerElement =document.getElementById("question-container")
 const questionElement =document.getElementById("question")
-const answerButtonElements =document.getElementById("answer-buttons")
+const answerButtonsElement =document.getElementById("answer-buttons")
 
 let shuffedQuestions,currectQuestionIndex;
 let quizScore=0;
+
+
+startButton.addEventListener("click",startGame)
+
+nextButton.addEventListener("click",() =>{
+    currectQuestionIndex++
+    setnextQuestion
+})
+
+
+function startGame(){
+    startButton.classList.add('hide')
+    shuffedQuestions=questions.sort(()=> Math.random() -0.5)
+    currectQuestionIndex=0;
+    questionContainerElement.classList.remove("hide")
+    setnextQuestion()
+    quizScore=0
+}
+
+
+
+
+function setnextQuestion(){
+    resetState();
+    showQuestion(shuffedQuestions[currectQuestionIndex])
+}
+
+
+function showQuestion(question){
+    questionElement.innerText=question.question;
+    question.answers.forEach((answer)=>{
+        const button = document.createElement('button')
+        button.innerText=answer.text;
+        button.classList.add('btn')
+        if(answer.correct){
+            button.dataset.correct=answer.correct
+        }
+        button.addEventListener('click',selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+function resetState(){
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while(answerButtonsElement.firstChild){
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+}
 
 
 function selectAnswer(e){
     const selectedButton=e.target
     const correct=selectedButton.dataset.correct
 
-    setStatusClass(document.body.correct)
-    Array.from()
+    setStatusClass(document.body,correct)
+    Array.from(answerButtonsElement.children).forEach((button)=>{
+        setStatusClass(button,button.dataset.correct)
+    })
+    if(shuffedQuestions.length>currectQuestionIndex +1){
+        nextButton.classList.remove("hide")
+    }else{
+        startButton.innerText ="restart"
+        startButton.classList.remove('hide')
+    }
+    if(selectedButton.dataset=correct){
+        quizScore++
+    }
+    document.getElementById('right-answers').innerText=quizScore
 }
+
 
 function setStatusClass(element,correct){
     clearStatusClass(element)
@@ -32,32 +94,32 @@ function clearStatusClass(element){
     element.classsList.remove('correct')
     element.classsList.remove('wrong')
 }
-const question=[
+const questions=[
+     {
+        question:"Which one of these is Javascript farmewor",
+        answers :[
+            {text:'python',correct:false},
+            {text:'react',correct:true},
+            {text:'django',correct:false},
+            {text:'ecillipse',correct:false}
+        ],
+    },
     {
-    question:"Which one of these is Javascript farmewor",
-    answers :[
-        {text:'python',correct:false},
-        {text:'react',correct:true},
-        {text:'django',correct:false},
-        {text:'ecillipse',correct:false},
-    ],
-},
-{
-    question:"Who is prime minister of india",
-    answers :[
-        {text:'Narendra modi',correct:true},
-        {text:'nehru',correct:true},
-        {text:'django',correct:false},
-        {text:'ecillipse',correct:false},
-    ],
-},
-{
-    question:"3x4=?",
-    answers :[
-        {text:'3',correct:false},
-        {text:'12',correct:true},
-        {text:'4',correct:false},
-        {text:'18',correct:false},
-    ],
-},
+        question:"Who is prime minister of india",
+        answers :[
+            {text:'Narendra modi',correct:true},
+            {text:'nehru',correct:true},
+            {text:'django',correct:false},
+            {text:'ecillipse',correct:false}
+        ],
+    },
+    {
+        question:"3x4=?",
+        answers :[
+            {text:'3',correct:false},
+            {text:'12',correct:true},
+            {text:'4',correct:false},
+            {text:'18',correct:false}
+        ],
+    },
 ]
